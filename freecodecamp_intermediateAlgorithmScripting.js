ef47f0de6df8199583c8471cf5f77a498748139a
Dizziby <Dizzi_by@mail.ru>
2022-03-08 18:47:27 +0300

    Task Intermediate Algorithm Scripting 1-5

--- freecodecamp_intermediateAlgorithmScripting.js
+++ freecodecamp_intermediateAlgorithmScripting.js
@@ -21,6 +21,20 @@ sumAll([1, 4]);
 
 // 2---Diff Two Arrays
 
+function diffArray(arr1, arr2) {
+    const newArr = [];
+    arr1.forEach(el => {
+        if(arr2.indexOf(el) === -1) {
+            newArr.push(el);
+        }});
+    arr2.forEach(el => {
+        if(arr1.indexOf(el) === -1) {
+            newArr.push(el);
+        }});
+    return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 
 
 // 3---Seek and Destroy
